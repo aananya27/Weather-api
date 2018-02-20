af006cf7ef5477fbc5821c5ff53fea37b75d5072
aananya <astream.h@outlook.com>
2018-02-20 17:19:44 +0530

    altered site to open weather

--- weather.js
+++ weather.js
@@ -1,34 +1,17 @@
 
+var weather = new XMLHttpRequest();
+var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=d183b59c195865a2b32f2fc527b35cfd'
+var data = {
+  //  id:524901,
+  //  APPID:"d183b59c195865a2b32f2fc527b35cfd",
+    q : " Portland ",
+    units : "metric"
+    };
 
-var my_city="Washington,USA";
-var my_key="2dce10dc411041708fb200431181902";
-var no_of_days=2;
-//build URI:
-var uri="http://free.worldweatheronline.com/feed/weather.ashx?q="+my_city+"&key="+my_key+"&format=json&no_of_days="+no_of_days+"&includeLocation=yes";
-// uri-encode it to prevent errors :
-uri=encodeURI(uri);
-
-jQuery.get(uri,function(r){},"json");
-
-var max_temp_C      =r.data.weather[0].tempMaxC;
-var min_temp_C      =r.data.weather[0].tempMinC;
-var   humidity        =r.data.current_condition[0].humidity;
-var precipitation   =r.data.current_condition[0].precipMM;
-
-  jQuery.get(uri,function(r){
-
- if(r.data.error) {
-
-  // error exists, so display it
-  alert(r.data.error[0].msg);
-
-  } else {
-    function showWeather(weatherReport) {
-      $('#temperature').text(weatherReport.main.temp);
-        }
-   // no error
-   // do normal stuff
-
- }//end if
-
-},"jsonp");
+function showWeather(weatherReport) {
+      $('#temperature').text(weatherReport.main);
+// console.log(q);
+      }
+$.get(weatherAPI , data , showWeather);
+ //$.getJSON(weatherAPI , data , showWeather);
+// showWeather();
