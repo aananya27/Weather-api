db01664c181815f30fbcf137c3e98dbbac86abae
aananya <astream.h@outlook.com>
2018-02-27 18:47:50 +0530

    closed #4

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@
 var weather = new XMLHttpRequest();
 var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=d183b59c195865a2b32f2fc527b35cfd'
 var data = {
-    q : " Portland ",
+    q : " delhi ",
     units : "metric"
     };
 
