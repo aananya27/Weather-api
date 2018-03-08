46f3a3204a5aab95b0683c0185476d677230c5c3
aananya <astream.h@outlook.com>
2018-03-08 18:05:04 +0530

    Closed #3

--- weather.js
+++ weather.js
@@ -1,21 +1,28 @@
 
 var weather = new XMLHttpRequest();
 var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=d183b59c195865a2b32f2fc527b35cfd'
-var cityName = prompt("Please enter your city Name.")
-$('h2.cityname').text(cityName);
+function cityWeather(q){
+
 var data = {
-    q : cityName ,
+    q : q ,
     units : "metric"
     };
-
 function showWeather(weatherReport) {
+    $('#city1').text(weatherReport.list[0].main.name);
     $('#temperature1').text(weatherReport.list[0].main.temp);
     $('#humid1').text(weatherReport.list[0].main.humidity);
     $('#description1').text(weatherReport.list[0].weather[0].description);
-//function that takes city name,returns temp,hum,desc.
-
-
-	console.log(weatherReport);
     }
 
 $.get(weatherAPI , data , showWeather);
+
+}
+
+for (var i = 0; i < 5; i++) {
+
+  var cityName = ["Delhi" , "Mumbai" , "Chennai" , "Vellore" , "Jaipur"];
+
+  cityWeather(cityName[i]);
+  console.log(cityName[i]);
+
+}
